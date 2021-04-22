0b9716c1b84a0705ea4d9fa825a73cb6fbbc1dc2
Fabrice Jammes <fabrice.jammes@k8s-school.fr>
2021-04-22 14:45:11 +0200

    upd

--- prereq.sh
+++ prereq.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+NB_USER=13
+
 sudo apt-get update
 sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg     lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -9,5 +11,11 @@ echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo useradd k8s1 --create-home --groups docker --shell /bin/bash
-sudo useradd k8s2 --create-home --groups docker --shell /bin/bash
+
+for i in {1..$NB_USER}
+do
+  USER="k8s${i}"
+  id -u $USER &>/dev/null || sudo useradd "$USER" --create-home --groups docker --shell /bin/bash
+  echo $USER:pinpin | sudo chpasswd
+done
+
