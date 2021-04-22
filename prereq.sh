3a63945c18da4e41d3f309686218fe3f233cab70
Fabrice Jammes <fabrice.jammes@k8s-school.fr>
2021-04-22 14:55:50 +0200

    upd

--- prereq.sh
+++ prereq.sh
@@ -4,17 +4,18 @@ set -euxo pipefail
 
 NB_USER=13
 
-sudo apt-get update
-sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+#sudo apt-get update
+#sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg     lsb-release
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt-get update
+#sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-for i in {1..$NB_USER}
+for ((i=1; i<=$NB_USER; i++))
 do
   USER="k8s${i}"
+  echo $USER
   id -u $USER &>/dev/null || sudo useradd "$USER" --create-home --groups docker --shell /bin/bash
   echo $USER:pinpin | sudo chpasswd
 done
