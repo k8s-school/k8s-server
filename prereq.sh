32bebd5fa8c0ecd477c90f18f526a6111d38c88a
Fabrice Jammes <fabrice.jammes@k8s-school.fr>
2021-04-22 14:59:25 +0200

    upd

--- prereq.sh
+++ prereq.sh
@@ -2,6 +2,10 @@
 
 set -euxo pipefail
 
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+. $DIR/env.sh
+
 NB_USER=13
 
 #sudo apt-get update
@@ -17,6 +21,6 @@ do
   USER="k8s${i}"
   echo $USER
   id -u $USER &>/dev/null || sudo useradd "$USER" --create-home --groups docker --shell /bin/bash
-  echo $USER:pinpin | sudo chpasswd
+  echo "${USER}:${i}${PASS}" | sudo chpasswd
 done
 
