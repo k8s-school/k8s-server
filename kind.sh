4fec77130dce526de9c6073344a30967237c8305
Fabrice Jammes <fabrice.jammes@k8s-school.fr>
2022-03-30 00:02:21 +0200

    upd

--- kind.sh
+++ kind.sh
@@ -6,6 +6,9 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 . $DIR/env.sh
 
+# WARN: Docker error
+# 429 Too Many Requests - Server message: toomanyrequests: You have reached your pull rate limit. You may increase the limit by authenticating and upgrading: https://www.docker.com/increase-rate-limit
+
 for ((i=0; i<=$NB_USER; i++))
 do
     "$HOME/kind-helper/k8s-create.sh" -p -c "calico" -n "k8s$i"
