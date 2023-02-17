420bc33ea2e705b82a376d16245243d535309624
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-02-17 19:31:32 +0100

    Update current feature

--- install-docker.TESTME.sh
+++ install-docker.TESTME.sh
@@ -11,3 +11,10 @@ sudo apt-get update
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -a -G docker k8s0
+
+echo "Clone sources"
+sudo apt install git
+git clone https://github.com/k8s-school/kind-helper.git
+git clone https://github.com/k8s-school/k8s-toolbox.git
+
