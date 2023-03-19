c2e1c207d96cdb21697826361f8d2ca516a5b9f1
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-03-19 21:22:48 +0100

    Update current feature

--- 0_prereq_install.sh
+++ 0_prereq_install.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove docker docker-engine docker.io containerd runc || echo "WARN no docker install found"
 sudo apt update
 sudo apt-get install     ca-certificates     curl     gnupg     lsb-release
 sudo mkdir -m 0755 -p /etc/apt/keyrings
