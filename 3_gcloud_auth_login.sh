8d36914046c805c5a72d100cc4479e3aac32faf1
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-03-26 20:59:31 +0200

    Update current feature

---
+++ 3_gcloud_auth_login.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+. $DIR/env.sh
+
+for ((i=1; i<=$NB_USER; i++))
+do
+    USER="k8s$i"
+    sudo su - "$USER" -c "~/k8s/toolbox.sh -C 'gcloud auth login'"
+done"
