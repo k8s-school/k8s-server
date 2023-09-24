e77bcb07d9b6b9b8eaccb8a944a57e3659a4b37d
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-09-24 23:56:36 +0200

    Update current feature

--- bootstrap/3_gcloud_auth_login.sh
+++ bootstrap/3_gcloud_auth_login.sh
@@ -6,8 +6,22 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 . $DIR/env.sh
 
+cat <<EOF > /tmp/gcloud-login.sh
+#!/bin/bash
+
+set -x
+
+sh -c "\$(k8s-toolbox desk --show) gcloud auth login"
+sh -c "\$(k8s-toolbox desk --show) gcloud config set project coastal-sunspot-206412"
+sh -c "\$(k8s-toolbox desk --show) gcloud compute instances list"
+
+EOF
+
+chmod +x /tmp/gcloud-login.sh
+
 for ((i=1; i<=$NB_USER; i++))
 do
     USER="k8s$i"
-    sudo su - "$USER" -c "$(k8s-toolbox desk --show) gcloud auth login"
-done"
+    export HOME="/home/$USER"
+    sudo su - "$USER" -c "/tmp/gcloud-login.sh"
+done
