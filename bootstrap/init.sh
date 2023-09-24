5710ee5a10cdcce597ed3ed5fb684f21c55e1751
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-09-24 18:28:34 +0200

    Update current feature

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -6,13 +6,17 @@ apt-get update -y
 apt-get install -y bash-completion git docker.io
 
 user="k8s0"
-pass=""
+pass="changeme"
 
-adduser "$user"
+adduser --disabled-password --gecos "" "$user"
 sudo usermod -a -G docker "$user"
 su - "$user" -c "git clone https://github.com/k8s-school/ikoula-setup.git"
 echo "$user:$pass" | chpasswd
 
-# Add sudo access without password
+echo "Add sudo access without password"
 echo "$user ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/"$user"
+
+echo "Setup sshd"
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl sshd restart
 
