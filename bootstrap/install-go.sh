b5e7092064dcf7ad6f2edbde65cc9a65493cc521
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-12-07 09:58:23 +0100

    Update current feature

---
+++ bootstrap/install-go.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+GO_VERSION="1.21.5"
+
+# Install Go
+sudo rm -rf /usr/local/go
+curl -sSL "https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz" | sudo tar -C /usr/local -xz
+export PATH=/usr/local/go/bin:$PATH
