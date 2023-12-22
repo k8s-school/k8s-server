d50bdb19d29d5d48226b3748231267ab9860703e
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-12-22 14:50:09 +0100

    Update current feature

--- bootstrap/generic/install-ciux.sh
+++ bootstrap/generic/install-ciux.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 # Install kubectl and setup auto-completion
-go install github.com/k8s-school/ciux@v1.1.1-rc6
+go install github.com/k8s-school/ciux@v0.0.1-rc11
 sudo cp "$HOME/go/bin/ciux" "/usr/local/bin"
