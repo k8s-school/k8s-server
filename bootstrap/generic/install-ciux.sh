45dfb0f6812496f25e5e74ab1c8472596bbcef4c
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-04-19 14:07:22 +0200

    Update current feature

--- bootstrap/generic/install-ciux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-# Install kubectl and setup auto-completion
-go install github.com/k8s-school/ciux@v0.0.1-rc11
-sudo cp "$HOME/go/bin/ciux" "/usr/local/bin"
---
+++ bootstrap/generic/install-godep.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+. "$DIR"/conf.sh
+
+mkdir -p $HOME/src
+
+# Install kubectl and setup auto-completion
+go install github.com/k8s-school/ktbx@v1.1.1-rc17
+sudo cp "$HOME/go/bin/ktbx" "/usr/local/bin"
+go install -v github.com/k8s-school/ink@v0.0.1-rc3
+sudo cp "$HOME/go/bin/ink" "/usr/local/bin"
+
+ktbx install kind
+ktbx install kubectl
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+
+# Setup kubectl aliases
+curl -Lo $HOME/.kubectl_aliases https://raw.githubusercontent.com/ahmetb/kubectl-alias/master/.kubectl_aliases
+echo '[ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases' >>~/.bashrc
+
+mkdir -p $HOME/.kube
+mkdir -p $HOME/.ktbx/homefs
