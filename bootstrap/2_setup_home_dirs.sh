b6c0186b8226119b476262fff5a04ceda65cc34a
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-04-21 18:40:06 +0200

    Update current feature

---
+++ bootstrap/2_setup_home_dirs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+. $DIR/env.sh
+
+
+for ((i=1; i<=$NB_USER; i++))
+do
+    USER="k8s$i"
+    GCLOUD_CONFIG="/home/$USER/.ktbx/homefs/.config"
+
+    if [ -d "$GCLOUD_CONFIG" ]; then
+      sudo cp -prf "$GCLOUD_CONFIG" /tmp
+    fi
+    sudo rm -rf /home/$USER
+    sudo mkdir -p "/home/$USER/.kube"
+    sudo chown -R $USER:$USER "/home/$USER"
+    sudo mkdir -p "/home/$USER/.ktbx/homefs"
+    sudo chown -R $USER:$USER "/home/$USER/.ktbx/homefs"
+
+    if [ -d "/tmp/.config" ]; then
+      sudo cp -prf /tmp/.config $GCLOUD_CONFIG
+      sudo rm -rf /tmp/.config
+    fi
+    sudo su $USER sh -c "git clone https://github.com/k8s-school/k8s-school /home/$USER/k8s-school"
+done
