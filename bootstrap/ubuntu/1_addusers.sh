006f2520959d25fa18cc07fcb03fd0effad656a1
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-05-24 18:14:05 +0200

    Update current feature

--- bootstrap/ubuntu/1_addusers.sh
+++ bootstrap/ubuntu/1_addusers.sh
@@ -6,11 +6,13 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 . $DIR/../env.sh
 
-for ((i=1; i<=$NB_USER; i++))
+for ((i=0; i<=$NB_USER; i++))
 do
   USER="k8s${i}"
   echo $USER
   id -u $USER &>/dev/null || sudo useradd "$USER" --create-home --groups docker --shell /bin/bash
   echo "${USER}:${i}${PASS}" | sudo chpasswd
+  sudo su "$USER" -c "mkdir -p /home/$USER/.kube && \
+  mkdir -p /home/$USER/.ktbx/homefs"
 done
 
