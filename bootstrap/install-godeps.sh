d35180c6ad0702085aa865bf4f016aeb9c3f3f10
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-06-09 22:05:35 +0200

    Update current feature

--- bootstrap/install-godeps.sh
+++ bootstrap/install-godeps.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Go may just have been installed
-. $HOME/.bashrc
+export PATH=/usr/local/go/bin:$PATH
 
 mkdir -p $HOME/src
 
