7fdeb484b6f30adcf09ded8f6832830f582e7998
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-06-15 23:11:41 +0200

    Update current feature

--- bootstrap/fedora/run_all.sh
+++ bootstrap/fedora/run_all.sh
@@ -6,12 +6,10 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 $DIR/../install-go.sh
 $DIR/../install-godeps.sh
+$DIR/../upgrade-sysctl.sh
+$DIR/0.1_install_docker.sh
 $DIR/1_addusers.sh
 $DIR/../2_setup_home_dirs.sh
-
-$DIR/../upgrade-sysctl.sh
-
-$DIR/0.1_install_docker.sh
 ktbx create
 
 # TODO
