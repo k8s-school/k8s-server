3cf05916ea9693157c1726470235d6bdd7180e50
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-06-15 23:05:44 +0200

    Update current feature

--- bootstrap/fedora/run_all.sh
+++ bootstrap/fedora/run_all.sh
@@ -5,8 +5,6 @@ set -euxo pipefail
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 $DIR/../install-go.sh
-# Source path to golang
-. ~/.bashrc
 $DIR/../install-godeps.sh
 $DIR/1_addusers.sh
 $DIR/../2_setup_home_dirs.sh
