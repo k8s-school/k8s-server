19d5bae5d10401ea1885c43612ebb340e57c6a6d
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-06-16 18:27:25 +0200

    Update current feature

--- bootstrap/docker_load.TODO.sh
+++ bootstrap/docker_load.TODO.sh
@@ -9,9 +9,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 NGINX_VERSION=1.25.3
 NGINX_IMAGE="nginx:$NGINX_VERSION"
 
+docker pull docker.io/k8sschool/k8s-toolbox:latest
 docker pull "$NGINX_IMAGE"
-docker save "$NGINX_IMAGE" > "/tmp/nginx.tar"
-IP=$($DIR/../infra/scw/get_ip.sh)
-scp "/tmp/nginx.tar" root@$IP:/tmp
-ssh root@"$IP" "docker load --input /tmp/nginx.tar"
-ssh root@"$IP" "kind load docker-image $NGINX_IMAGE"
+kind load docker-image "$NGINX_IMAGE"
+
+# From workstation:
+# docker save "$NGINX_IMAGE" > "/tmp/nginx.tar"
+# IP=$($DIR/../infra/scw/get_ip.sh)
+# scp "/tmp/nginx.tar" root@$IP:/tmp
+# ssh root@"$IP" "docker load --input /tmp/nginx.tar"
