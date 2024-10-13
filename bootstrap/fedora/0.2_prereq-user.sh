0c4c69a7d372420f883368bb37d802f49bd206cc
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-10-13 14:37:07 +0200

    Update current feature

--- bootstrap/fedora/0.2_prereq-user.sh
+++ bootstrap/fedora/0.2_prereq-user.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 . "$DIR"/../env.sh
 
-sudo dnf install -y byobu
+sudo dnf install -y byobu lsb_release
 
 mkdir -p $HOME/src
 
