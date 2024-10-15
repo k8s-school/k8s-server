58d41ea6db6f3c860b90c24f1dfd4b810db7e003
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-10-15 10:43:46 +0200

    Update current feature

--- bootstrap/gcloud_cleanup.sh
+++ bootstrap/gcloud_cleanup.sh
@@ -4,6 +4,7 @@
 
 set -euxo pipefail
 
+echo "WARN: use k8s-student account!!!"
 for i in $(gcloud compute os-login ssh-keys list --format="table[no-heading](value.fingerprint)"); do
   echo $i;
   gcloud compute os-login ssh-keys remove --key $i || true;
