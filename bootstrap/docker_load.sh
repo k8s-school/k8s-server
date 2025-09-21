d30be5c3e292466cfd8c7ea33d7f2be9b8a4bc64
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2025-09-21 17:32:42 +0200

    Improve docker pre-loading

--- bootstrap/docker_load.sh
+++ bootstrap/docker_load.sh
@@ -11,17 +11,26 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 NGINX_VERSION=1.25.3
 NGINX_IMAGE="nginx:$NGINX_VERSION"
 
-docker login
-docker pull k8sschool/kubectl-proxy:1.27.3
-docker pull curlimages/curl
-docker pull mongo:3.4.1
-docker pull ubuntu:24.04
-docker pull "$NGINX_IMAGE"
-docker pull docker.io/bitnami/postgresql:14.5.0-debian-11-r14
+IMAGES=(
+  "$NGINX_IMAGE"
+  "k8sschool/kubectl-proxy:1.27.3"
+  "curlimages/curl"
+    "mongo:3.4.1"
+    "ubuntu:24.04"
+    "docker.io/bitnami/postgresql:14.5.0-debian-11-r14"
+    "docker.io/k8sschool/k8s-toolbox:latest"
 
-docker pull docker.io/k8sschool/k8s-toolbox:latest
-docker pull "$NGINX_IMAGE"
-kind load docker-image "$NGINX_IMAGE"
+)
+
+docker login
+for IMAGE in "${IMAGES[@]}"; do
+  docker pull "$IMAGE"
+done
+
+for IMAGE in "${IMAGES[@]}"; do
+  kind load docker-image "$IMAGE"
+done
+
 
 # From workstation:
 # docker save "$NGINX_IMAGE" > "/tmp/nginx.tar"
