59c668db2f55ebc9ba5128b5f0a236f20bd40831
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2026-01-18 17:16:55 +0100

    Update current feature

--- bootstrap/upgrade-sysctl.sh
+++ bootstrap/upgrade-sysctl.sh
@@ -9,8 +9,8 @@ set -euxo pipefail
 # Add these to /etc/sysctl.conf
 cat <<EOF | sudo tee -a /etc/sysctl.conf
 # See https://github.com/kubernetes-sigs/kind/issues/2219
-fs.inotify.max_user_watches=524288
-fs.inotify.max_user_instances=512
+fs.inotify.max_user_watches=1048576
+fs.inotify.max_user_instances=1024
 EOF
 
 # Apply the changes
