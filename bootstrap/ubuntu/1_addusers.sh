34c804aa2e8d45a453d52485902ad862229c04ab
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2026-01-19 21:46:30 +0100

    Update current feature

--- bootstrap/ubuntu/1_addusers.sh
+++ bootstrap/ubuntu/1_addusers.sh
@@ -10,7 +10,7 @@ for ((i=0; i<=$NB_USER; i++))
 do
   USER="k8s${i}"
   echo $USER
-  id -u $USER &>/dev/null || sudo useradd "$USER" --create-home --groups docker --shell /bin/bash
+  id -u $USER &>/dev/null || sudo useradd "$USER" --create-home --groups docker,adm --shell /bin/bash
   echo "${USER}:${i}${PASS}" | sudo chpasswd
   sudo su "$USER" -c "mkdir -p /home/$USER/.kube && \
   mkdir -p /home/$USER/.ktbx/homefs"
